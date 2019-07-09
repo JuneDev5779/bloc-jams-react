92b07fd499fa018c8a09c051cbff06a620722e3c
Junibel De La Cruz <junibeldelacruz@gmail.com>
2019-07-09 11:05:45 -0600

    Checkpoint 54 components library: completed

--- src/App.js
+++ src/App.js
@@ -11,15 +11,15 @@ function App() {
       <header>
         <nav>
           <Link to='/'>Landing </Link>
-          <Link to='/library'>Library</Link>
-          <Link to='/album'>Album</Link>
+          <Link to='/library'>- Library</Link>
+          <Link to='/album'> - Album </Link>
         </nav>
         <h1>Bloc Jams</h1>
       </header>
       <main>
         <Route exact path="/" component={Landing} />
         <Route path="/library" component={Library} />
-        <Route path="/album" component={Album} />
+        <Route path="/album/:slug" component={Album} />
       </main>  
     </div>
   );
